4eb02446c095a2c584492dae0f0ea4ef853da68e
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-27 21:49:29 -0300

    wip

--- src/drivers/http_requester.py
+++ src/drivers/http_requester.py
@@ -1,11 +1,11 @@
 import requests
 
 class HttpRequester:
-
+    #onde estiver self esta no mesmo contexto
     def __init__(self) -> None:
         self.__url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
         # privado
 
-    def request_from_page(self):
-        response = requests.get
+    def request_from_page(self): 
+        response = requests.get(self.__url)
         print(response)
--- src/drivers/http_requester_test.py
+++ src/drivers/http_requester_test.py
@@ -1,4 +1,4 @@
-from .http_requester import HttpRequester
+from .http_requester import HttpRequester #deixar como o nome do arquivo
 
 def test_request_from_page():
     http_requester = HttpRequester()
