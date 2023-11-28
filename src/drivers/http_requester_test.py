1324026175df11473ccd56f7caa503192247e30f
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-11-27 21:55:52 -0300

    wip

--- src/drivers/http_requester_test.py
+++ src/drivers/http_requester_test.py
@@ -1,5 +1,5 @@
-from .http_requester import HttpRequester #deixar como o nome do arquivo
+from .http_requester import HttpRequester #importar o arquivo a ser testado
 
-def test_request_from_page():
+def test_request_from_page(): #sempre ter test no começo / nome da classe a ser testada
     http_requester = HttpRequester()
-    http_requester
+    http_requester.request_from_page()
